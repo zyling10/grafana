355de60a0afe66cb10e52356f2a1cf6efe108686
Kristian Bremberg <kristian.bremberg@grafana.com>
2023-02-02 15:52:30 +0100

    Fixed failing test file

--- packages/grafana-data/src/text/sanitize.test.ts
+++ packages/grafana-data/src/text/sanitize.test.ts
@@ -6,7 +6,7 @@ describe('Sanitize wrapper', () => {
       '<div style="display:flex; flex-direction: column; flex-wrap: wrap; justify-content: start; gap: 2px;"><div style="flex-basis: 50%"></div></div>';
     const str = sanitizeTextPanelContent(html);
     expect(str).toBe(
-      '<div style="display:flex; flex-direction:column; flex-wrap:wrap; justify-content:start; gap:2px;"><div style="flex-basis:50%;"></div></div>'
+      '<div style="display:flex; flex-direction: column; flex-wrap: wrap; justify-content: start; gap: 2px;"><div style="flex-basis: 50%"></div></div>'
     );
   });
 });
