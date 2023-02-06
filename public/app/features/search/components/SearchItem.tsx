ac3f0586b84140bfa248e0e822c7ed40b60d814f
joshhunt <josh@trtr.co>
2023-02-06 16:10:20 +0000

    dirty dirty code for showing nested folders in folder view
    refactor to NestedFolderItem
    
    Update dashboard grid view to new types
    
    update tests
    
    REBASE OUT OF THIS BRANCH - joshhunt/star-by-uid merged into this
    
    Squashed commit of the following:
    
    commit d0f046ccd3d820575f58d9a60cfcedf5cbdb217d
    Author: joshhunt <josh@trtr.co>
    Date:   Wed Feb 8 18:35:56 2023 +0000
    
        undo async
    
    commit abe2777a1fdb6418802102fbb1b6aca7ae4d8e66
    Author: joshhunt <josh@trtr.co>
    Date:   Wed Feb 8 18:34:11 2023 +0000
    
        Dashboards: Star dashboards by UID
    
    add type for dashboard search dto
    
    clean DashboardSearchItem type
    
    simplify DashboardSearchHit type
    
    remove unused properties from DashboardSearchHit
    
    make uid non-optional
    
    rename + move NestedFolderItem type to DashboardViewItem
    
    clean up

--- public/app/features/search/components/SearchItem.tsx
+++ public/app/features/search/components/SearchItem.tsx
@@ -63,7 +63,10 @@ export const SearchItem: FC<Props> = ({ item, isSelected, editable, onToggleChec
       className={styles.container}
       onClick={onClickItem}
     >
-      <Card.Heading>{item.title}</Card.Heading>
+      <Card.Heading>
+        <KindIcon kind={item.kind} /> {item.title}
+      </Card.Heading>
+
       <Card.Figure align={'center'} className={styles.checkbox}>
         <SearchCheckbox
           aria-label="Select dashboard"
@@ -72,6 +75,7 @@ export const SearchItem: FC<Props> = ({ item, isSelected, editable, onToggleChec
           onClick={handleCheckboxClick}
         />
       </Card.Figure>
+
       <Card.Meta separator={''}>
         <span className={styles.metaContainer}>
           <Icon name={getIconForKind(item.parentKind ?? 'folder')} aria-hidden />
@@ -91,6 +95,14 @@ export const SearchItem: FC<Props> = ({ item, isSelected, editable, onToggleChec
     </Card>
   );
 };
+
+function KindIcon({ kind }: { kind: DashboardViewItem['kind'] }) {
+  if (kind === 'folder') {
+    return <Icon name="folder" />;
+  }
+
+  return null;
+}
 
 const getStyles = (theme: GrafanaTheme2) => {
   return {
