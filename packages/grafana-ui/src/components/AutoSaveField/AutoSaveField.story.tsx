f3926e01e87332d3b6b2ad8897b9ca3c88585371
eledobleefe <laura.fernandez@grafana.com>
2023-02-07 20:23:23 +0100

    Add more types of input to the story

--- packages/grafana-ui/src/components/AutoSaveField/AutoSaveField.story.tsx
+++ packages/grafana-ui/src/components/AutoSaveField/AutoSaveField.story.tsx
@@ -1,8 +1,11 @@
+import { useArgs } from '@storybook/client-api';
 import { Story, Meta } from '@storybook/react';
-import React from 'react';
+import React, { useState } from 'react';
 
 import { withCenteredStory } from '../../utils/storybook/withCenteredStory';
 import { WeekStartPicker } from '../DateTimePickers/WeekStartPicker';
+import { Checkbox } from '../Forms/Checkbox';
+import { RadioButtonGroup } from '../Forms/RadioButtonGroup/RadioButtonGroup';
 import { Input } from '../Input/Input';
 
 import { AutoSaveField } from './AutoSaveField';
@@ -17,7 +20,17 @@ const meta: Meta = {
       page: mdx,
     },
     controls: {
-      exclude: ['prefix', 'width', 'loading', 'suffix', 'addonBefore', 'addonAfter', 'onFinishChange', 'invalid'],
+      exclude: [
+        'prefix',
+        'width',
+        'loading',
+        'suffix',
+        'addonBefore',
+        'addonAfter',
+        'onFinishChange',
+        'invalid',
+        'weekPickerValue',
+      ],
     },
   },
   argTypes: {
@@ -31,6 +44,7 @@ const meta: Meta = {
     saveErrorMessage: 'This is a custom error message',
     label: 'Custom label',
     required: false,
+    themeOptions: '',
   },
 };
 
@@ -47,26 +61,119 @@ const getError = () => {
   });
 };
 
-export const AutoSaveFieldError: Story = (args) => (
-  <AutoSaveField onFinishChange={getError} {...args}>
-    <Input />
-  </AutoSaveField>
-);
+export const AutoSaveFieldError: Story = (args) => {
+  const themeOptions = [
+    { value: '', label: 'Default' },
+    { value: 'dark', label: 'Dark' },
+    { value: 'light', label: 'Light' },
+    { value: 'system', label: 'System' },
+  ];
+  const [selected, setSelected] = useState('');
+  const [autoFit, setAutofit] = useState(false);
+
+  return (
+    <div>
+      <AutoSaveField onFinishChange={getError} {...args}>
+        {(onChange) => <Input onChange={(e) => onChange(e.currentTarget.value)} />}
+      </AutoSaveField>
+      <AutoSaveField onFinishChange={getError} label={'WeekStartPicker as child'}>
+        {(onChange) => (
+          <WeekStartPicker
+            value={args.weekPickerValue}
+            onChange={(weekPickerValue) => {
+              onChange(weekPickerValue);
+            }}
+          />
+        )}
+      </AutoSaveField>
+      <AutoSaveField onFinishChange={getError} label={'UI Theme'}>
+        {(onChange) => (
+          <RadioButtonGroup
+            options={themeOptions}
+            value={selected}
+            onChange={(themeOption) => {
+              setSelected(themeOption);
+              onChange(themeOption);
+            }}
+          />
+        )}
+      </AutoSaveField>
+      <AutoSaveField onFinishChange={getSuccess} label={'Checkbox'}>
+        {(onChange) => (
+          <Checkbox
+            label="Autofit"
+            description="Panel heights will be adjusted to fit screen size"
+            name="autofix"
+            value={autoFit}
+            onChange={(e) => {
+              const value = e.currentTarget.checked.toString();
+              onChange(value);
+              setAutofit(e.currentTarget.checked);
+            }}
+          />
+        )}
+      </AutoSaveField>
+    </div>
+  );
+};
 AutoSaveFieldError.args = {
   label: 'With error',
   required: false,
 };
 
-export const AutoSaveFieldSuccess: Story = (args) => (
-  <div>
-    <AutoSaveField onFinishChange={getSuccess} {...args}>
-      <Input />
-    </AutoSaveField>
-    <AutoSaveField onFinishChange={getSuccess} {...args}>
-      <WeekStartPicker />
-    </AutoSaveField>
-  </div>
-);
+export const AutoSaveFieldSuccess: Story = (args) => {
+  const themeOptions = [
+    { value: '', label: 'Default' },
+    { value: 'dark', label: 'Dark' },
+    { value: 'light', label: 'Light' },
+    { value: 'system', label: 'System' },
+  ];
+  const [autoFit, setAutofit] = useState(false);
+
+  return (
+    <div>
+      <AutoSaveField onFinishChange={getSuccess} {...args}>
+        {(onChange) => <Input onChange={(e) => onChange(e.currentTarget.value)} />}
+      </AutoSaveField>
+      <AutoSaveField onFinishChange={getSuccess} label="WeekStartPicker as child">
+        {(onChange) => (
+          <WeekStartPicker
+            value={args.weekPickerValue}
+            onChange={(newValue) => {
+              onChange(newValue);
+            }}
+          />
+        )}
+      </AutoSaveField>
+      <AutoSaveField onFinishChange={getSuccess} label={'UI Theme'}>
+        {(onChange) => (
+          <RadioButtonGroup
+            options={themeOptions}
+            value={args.themeOptions}
+            onChange={(newTheme) => {
+              onChange(newTheme);
+            }}
+          />
+        )}
+      </AutoSaveField>
+      <AutoSaveField onFinishChange={getSuccess} label={'Checkbox'}>
+        {(onChange) => (
+          <Checkbox
+            label="Autofit"
+            description="Panel heights will be adjusted to fit screen size"
+            name="autofix"
+            value={autoFit}
+            onChange={(e) => {
+              const value = e.currentTarget.checked.toString();
+              onChange(value);
+              setAutofit(e.currentTarget.checked);
+            }}
+          />
+        )}
+      </AutoSaveField>
+    </div>
+  );
+};
 AutoSaveFieldSuccess.args = {
   label: 'With success',
   required: true,
